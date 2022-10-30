ce0ed3abd012e956a9b11a4b5cacc71dd4b66a4b
shoebahmed <shoebahmed@yahoo.com>
2022-10-30 10:33:14 +0000

    Dynamic product list

---
+++ src/constants/products.ts
@@ -0,0 +1,109 @@
+import { INewProduct } from "./types";
+
+export const recentProductList: INewProduct[] = [
+    {
+      id:11, 
+      category:"Electornic",
+      location: "Flat 402",
+      quantity: 5,
+      description: "New Iphone purchased from MediaMarkt on 12-Oct-22",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:12, 
+      category:"Home Appliances",
+      location: "Flat 403",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:13, 
+      category:"phones",
+      location: "Flat 40",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+  ]
+
+  export const electronicProductList: INewProduct[] = [
+    {
+      id:14, 
+      category:"Electornic",
+      location: "Flat 402",
+      quantity: 5,
+      description: "New Iphone purchased from MediaMarkt on 12-Oct-22",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:15, 
+      category:"Home Appliances",
+      location: "Flat 403",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:16, 
+      category:"phones",
+      location: "Flat 40",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+  ]
+
+  export const homeApplianceProductList: INewProduct[] = [
+    {
+      id:17, 
+      category:"Electornic",
+      location: "Flat 402",
+      quantity: 5,
+      description: "New Iphone purchased from MediaMarkt on 12-Oct-22",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:18, 
+      category:"Home Appliances",
+      location: "Flat 403",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:19, 
+      category:"phones",
+      location: "Flat 40",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+  ]
+
+  export const industrialProductList: INewProduct[] = [
+    {
+      id:20, 
+      category:"Electornic",
+      location: "Flat 402",
+      quantity: 5,
+      description: "New Iphone purchased from MediaMarkt on 12-Oct-22",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:21, 
+      category:"Home Appliances",
+      location: "Flat 403",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+    {
+      id:22, 
+      category:"phones",
+      location: "Flat 40",
+      quantity: 10,
+      description: "New Iphone sold out",
+      image: 'https://images.unsplash.com/photo-1604998103924-89e012e5265a?fit=crop&w=450&q=80',
+    },
+  ]
