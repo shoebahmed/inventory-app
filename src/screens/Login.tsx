6af2ffbfba306a8d238e7e85c9df8dcbdefad65c
zaki <zaki@mazeqube.com>
2022-10-31 17:25:07 +0530

    Login Page Changes

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -65,153 +65,137 @@ const Login = () => {
 
   return (
     <Block safe marginTop={sizes.md}>
-      <Block paddingHorizontal={sizes.s}>
-        <Block flex={0} style={{zIndex: 0}}>
-          <Image
-            background
-            resizeMode="cover"
-            padding={sizes.sm}
-            radius={sizes.cardRadius}
-            source={assets.background}
-            height={sizes.height * 0.3}>
-            <Button
-              row
-              flex={0}
-              justify="flex-start"
-              onPress={() => navigation.goBack()}>
-              <Image
-                radius={0}
-                width={10}
-                height={18}
-                color={colors.white}
-                source={assets.arrow}
-                transform={[{rotate: '180deg'}]}
-              />
-              <Text p white marginLeft={sizes.s}>
-                {t('common.goBack')}
-              </Text>
-            </Button>
-
-            <Text h4 center white marginBottom={sizes.md}>
-              {t('register.title')}
-            </Text>
-          </Image>
-        </Block>
-        {/* register form */}
+    <Block paddingHorizontal={sizes.s}>
+      <Block flex={0} style={{zIndex: 0}}>
+        <Image
+          background
+          resizeMode="cover"
+          padding={sizes.sm}
+          radius={sizes.cardRadius}
+          source={assets.background}
+          height={sizes.height * 0.3}>
+          <Text h4 center white marginBottom={sizes.md}>
+            {t('register.title')}
+          </Text>
+        </Image>
+      </Block>
+      {/* register form */}
+      <Block
+        keyboard
+        behavior={!isAndroid ? 'padding' : 'height'}
+        marginTop={-(sizes.height * 0.2 - sizes.l)}>
         <Block
-          keyboard
-          behavior={!isAndroid ? 'padding' : 'height'}
-          marginTop={-(sizes.height * 0.2 - sizes.l)}>
+          flex={0}
+          radius={sizes.sm}
+          marginHorizontal="8%"
+          shadow={!isAndroid} // disabled shadow on Android due to blur overlay + elevation issue
+        >
           <Block
+            blur
             flex={0}
+            intensity={90}
             radius={sizes.sm}
-            marginHorizontal="8%"
-            shadow={!isAndroid} // disabled shadow on Android due to blur overlay + elevation issue
-          >
-            <Block
-              blur
-              flex={0}
-              intensity={90}
-              radius={sizes.sm}
-              overflow="hidden"
-              justify="space-evenly"
-              tint={colors.blurTint}
-              paddingVertical={sizes.sm}>
-              <Text p semibold center>
-                {t('register.subtitle')}
-              </Text>
-              {/* social buttons */}
-              <Block row center justify="space-evenly" marginVertical={sizes.m}>
-                <Button outlined gray shadow={!isAndroid}>
-                  <Image
-                    source={assets.facebook}
-                    height={sizes.m}
-                    width={sizes.m}
-                    color={isDark ? colors.icon : undefined}
-                  />
-                </Button>
-                <Button outlined gray shadow={!isAndroid}>
-                  <Image
-                    source={assets.apple}
-                    height={sizes.m}
-                    width={sizes.m}
-                    color={isDark ? colors.icon : undefined}
-                  />
-                </Button>
-                <Button outlined gray shadow={!isAndroid}>
-                  <Image
-                    source={assets.google}
-                    height={sizes.m}
-                    width={sizes.m}
-                    color={isDark ? colors.icon : undefined}
-                  />
-                </Button>
-              </Block>
-              <Block
-                row
-                flex={0}
-                align="center"
-                justify="center"
-                marginBottom={sizes.sm}
-                paddingHorizontal={sizes.xxl}>
-                <Block
-                  flex={0}
-                  height={1}
-                  width="50%"
-                  end={[1, 0]}
-                  start={[0, 1]}
-                  gradient={gradients.divider}
+            overflow="hidden"
+            justify="space-evenly"
+            tint={colors.blurTint}
+            paddingVertical={sizes.sm}>
+            <Text p semibold center>
+              {t('register.subtitle')}
+            </Text>
+            {/* social buttons */}
+            <Block row center justify="space-evenly" marginVertical={sizes.m}>
+              <Button outlined gray shadow={!isAndroid}>
+                <Image
+                  source={assets.facebook}
+                  height={sizes.m}
+                  width={sizes.m}
+                  color={isDark ? colors.icon : undefined}
                 />
-                <Text center marginHorizontal={sizes.s}>
-                  {t('common.or')}
-                </Text>
-                <Block
-                  flex={0}
-                  height={1}
-                  width="50%"
-                  end={[0, 1]}
-                  start={[1, 0]}
-                  gradient={gradients.divider}
+              </Button>
+              <Button outlined gray shadow={!isAndroid}>
+                <Image
+                  source={assets.apple}
+                  height={sizes.m}
+                  width={sizes.m}
+                  color={isDark ? colors.icon : undefined}
                 />
-              </Block>
-          
-                <Input
-                  autoCapitalize="none"
-                  marginBottom={sizes.m}
-                  label={t('common.email')}
-                  keyboardType="email-address"
-                  placeholder={t('common.emailPlaceholder')}
-                  success={Boolean(registration.email && isValid.email)}
-                  danger={Boolean(registration.email && !isValid.email)}
-                  onChangeText={(value) => handleChange({email: value})}
+              </Button>
+              <Button outlined gray shadow={!isAndroid}>
+                <Image
+                  source={assets.google}
+                  height={sizes.m}
+                  width={sizes.m}
+                  color={isDark ? colors.icon : undefined}
                 />
-                <Input
-                  secureTextEntry
-                  autoCapitalize="none"
-                  marginBottom={sizes.m}
-                  label={t('common.password')}
-                  placeholder={t('common.passwordPlaceholder')}
-                  onChangeText={(value) => handleChange({password: value})}
-                  success={Boolean(registration.password && isValid.password)}
-                  danger={Boolean(registration.password && !isValid.password)}
-                />
-              </Block>              
-              <Button
-                primary
-                outlined
-                shadow={!isAndroid}
-                marginVertical={sizes.s}
-                marginHorizontal={sizes.sm}
-                onPress={() => navigation.navigate('Pro')}>
-                <Text bold primary transform="uppercase">
-                  {t('common.signin')}
-                </Text>
               </Button>
             </Block>
+            <Block
+              row
+              flex={0}
+              align="center"
+              justify="center"
+              marginBottom={sizes.sm}
+              paddingHorizontal={sizes.xxl}>
+              <Block
+                flex={0}
+                height={1}
+                width="50%"
+                end={[1, 0]}
+                start={[0, 1]}
+                gradient={gradients.divider}
+              />
+              <Text center marginHorizontal={sizes.s}>
+                {t('common.or')}
+              </Text>
+              <Block
+                flex={0}
+                height={1}
+                width="50%"
+                end={[0, 1]}
+                start={[1, 0]}
+                gradient={gradients.divider}
+              />
+            </Block>
+            {/* form inputs */}
+            <Block paddingHorizontal={sizes.sm}>
+              <Input
+                autoCapitalize="none"
+                marginBottom={sizes.m}
+                label={t('common.email')}
+                keyboardType="email-address"
+                placeholder={t('common.emailPlaceholder')}
+                success={Boolean(registration.email && isValid.email)}
+                danger={Boolean(registration.email && !isValid.email)}
+                onChangeText={(value) => handleChange({email: value})}
+              />
+              <Input
+                secureTextEntry
+                autoCapitalize="none"
+                marginBottom={sizes.m}
+                label={t('common.password')}
+                placeholder={t('common.passwordPlaceholder')}
+                onChangeText={(value) => handleChange({password: value})}
+                success={Boolean(registration.password && isValid.password)}
+                danger={Boolean(registration.password && !isValid.password)}
+              />
+            </Block>
+            <Button
+              primary
+              outlined
+              shadow={!isAndroid}
+              marginVertical={sizes.s}
+              marginHorizontal={sizes.sm}
+              onPress={() => navigation.navigate('Home')}>
+              <Text bold primary transform="uppercase">
+                {t('common.signin')}
+              </Text>
+            </Button>
           </Block>
         </Block>
       </Block>
-  );
+    </Block>
+  </Block>
+);
 };
 
 export default Login;
